1533e32c1ebf1caee23908578887e3df6eb5deff
Aleksandar <aleksandar@e-vlasotince.info>
2024-06-30 15:02:03 +0200

    final

--- src/components/Messanger.jsx
+++ src/components/Messanger.jsx
@@ -5,15 +5,11 @@ import MessagesBox from "./MessagesBox";
 import InputBox from "./InputBox";
 
 const Messanger = () => {
-  const { isPending: isLoading, messages, error: errorFetch } = useMessages();
+  const { messages, isPending: isLoading, error } = useMessages();
 
   return (
     <div className="container mx-auto max-w-4xl flex-col sm:flex-row h-screen flex px-4 py-6 gap-4">
-      <MessagesBox
-        messages={messages}
-        isLoading={isLoading}
-        error={errorFetch}
-      />
+      <MessagesBox messages={messages} isLoading={isLoading} error={error} />
       <InputBox />
     </div>
   );
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,5 @@
 // eslint-disable-next-line react/prop-types
-// import { useFormStatus } from "react-dom";
-
-// eslint-disable-next-line react/prop-types
 const Button = ({ isPending }) => {
-  // const { pending } = useFormStatus();
   return (
     <button className="p-3 text-white  bg-orange-700   rounded-lg self-center">
       {isPending ? "Sending..." : "Send"}
